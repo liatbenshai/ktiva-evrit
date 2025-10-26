928946052225dec6decce45fb1ce759b8ee54933
liatbenshai <48071856+liatbenshai@users.noreply.github.com>
2025-10-26 19:05:50 +0200

    Refactor Card component and its subcomponents

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,45 +1,21 @@
 import * as React from "react"
 
-const Card = React.forwardRef
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={className}
-    {...props}
-  />
+const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
+  <div ref={ref} className={className} {...props} />
 ))
 Card.displayName = "Card"
 
-const CardHeader = React.forwardRef
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={className}
-    {...props}
-  />
+const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
+  <div ref={ref} className={className} {...props} />
 ))
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = React.forwardRef
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLHeadingElement>
->(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={className}
-    {...props}
-  />
+const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(({ className, ...props }, ref) => (
+  <h3 ref={ref} className={className} {...props} />
 ))
 CardTitle.displayName = "CardTitle"
 
-const CardContent = React.forwardRef
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
+const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
   <div ref={ref} className={className} {...props} />
 ))
 CardContent.displayName = "CardContent"
